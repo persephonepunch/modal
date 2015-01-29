8ed457795380a59264d5f551dbab917475a06ff8
yoonsun lee <yoonsunlee150@gmail.com>
2015-01-28 20:42:58 -0500

    updated modal constraints

---
+++ modal/PlayViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PlayViewController.swift
+//  modal
+//
+//  Created by YOONSUN LEE on 1/28/15.
+//  Copyright (c) 2015 YOONSUN LEE. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    @IBAction func CLOSE(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
